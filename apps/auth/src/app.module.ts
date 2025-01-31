d391114662de853e5aff49576debcb2df1ed2911
Grecco, Franco <srgrecco93@gmail.com>
2025-01-31 10:06:31 +0100

    refactor: rename config conventions

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { configModuleOptions } from './modules/configs/environment';
+import { configModuleOptions } from './modules/configs/config-module.config';
+import { typeOrmModuleOptions } from './modules/configs/typeorm.config';
 
 @Module({
     ConfigModule.forRoot(configModuleOptions),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
   controllers: [],
   providers: [],
 })
