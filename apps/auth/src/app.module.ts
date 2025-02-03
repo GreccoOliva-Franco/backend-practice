5a0f9ddb5b792525b866364ad03cef452c2b0b9e
Grecco, Franco <srgrecco93@gmail.com>
2025-02-03 22:00:08 +0100

    add auth module

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { configModuleOptions } from './modules/configs/config-module.config';
-import { typeOrmModuleOptions } from './modules/configs/typeorm.config';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtModuleAsyncOptions } from './modules/auth/jwt.constants';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleOptions } from './modules/configs/typeorm-config.config';
 
 @Module({
+  imports: [
     ConfigModule.forRoot(configModuleOptions),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    JwtModule.registerAsync(jwtModuleAsyncOptions),
+
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
