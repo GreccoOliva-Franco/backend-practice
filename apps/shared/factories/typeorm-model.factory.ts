739d49bd307c0fea409b1c3c10010d7a69b6fe1c
Grecco, Franco <srgrecco93@gmail.com>
2025-02-03 21:55:54 +0100

    add factory implementation for typeorm

--- apps/shared/factories/typeorm-model.factory.ts
+++ apps/shared/factories/typeorm-model.factory.ts
@@ -1,25 +1,9 @@
-import { ModelFactory } from './model-factory.interface';
+import { ModelFactory } from './model-factory.factory';
 
 export abstract class TypeOrmModelFactory<T> extends ModelFactory<T> {
   constructor(protected readonly model: T) {
     super(model);
   }
 
-  public makeOne(): T {
-    const properties = {
-      ...this.definition(),
-      ...this.options.state,
-    };
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore no comment
-    const instance = new this.model();
-    Object.keys(properties).forEach((prop) => {
-      instance[prop] = properties[prop];
-    });
-
-    return instance;
-  }
-
-  protected abstract definition(): Partial<T>;
+  protected abstract getAllProperties(): T;
 }
