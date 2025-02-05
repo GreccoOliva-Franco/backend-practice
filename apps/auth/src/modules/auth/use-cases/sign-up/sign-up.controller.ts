28622d49f48e6cc8d7dbcd0feae4bed5c5ffe50a
Grecco, Franco <srgrecco93@gmail.com>
2025-02-05 08:48:42 +0100

    ms.auth: define url path and consume it in every use-case controller

--- apps/auth/src/modules/auth/use-cases/sign-up/sign-up.controller.ts
+++ apps/auth/src/modules/auth/use-cases/sign-up/sign-up.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { URL_PATH } from '@apps/auth/modules/auth/constants/auth.constants';
 import { SignUpDto } from './dtos/sign-up.dto';
 import { SignUpService } from './sign-up.service';
 
-@Controller('auth/sign-up')
+@Controller(URL_PATH)
 export class SignUpController {
   constructor(private readonly service: SignUpService) {}
 
-  @Post()
+  @Post('sign-up')
   execute(@Body() signUpDto: SignUpDto) {
     return this.service.execute(signUpDto);
   }
