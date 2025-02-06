8ecd936d7ccf207fc77af3eddd95d5989ff76fb4
Grecco, Franco <srgrecco93@gmail.com>
2025-02-06 12:23:20 +0100

    ms.auth: add jwt globally

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -4,15 +4,16 @@ import { ConfigModule } from '@nestjs/config';
 import { configModuleOptions } from './modules/configs/config-module.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmModuleOptions } from './modules/configs/typeorm-config.config';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtModuleAsyncOptions } from './modules/configs/jwt-module.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configModuleOptions),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    JwtModule.registerAsync(jwtModuleAsyncOptions),
 
     AuthModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
