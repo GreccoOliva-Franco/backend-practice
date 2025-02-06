c6ab0d17b6008d8394f17d30566472fa35fdf5cc
Grecco, Franco <srgrecco93@gmail.com>
2025-02-06 06:57:21 +0100

    ms.auth: add auth.get-profile use-case

--- apps/auth/src/modules/auth/auth.module.ts
+++ apps/auth/src/modules/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SignUpModule } from './use-cases/sign-up/sign-up.module';
 import { SignInModule } from './use-cases/sign-in/sign-in.module';
+import { GetProfileModule } from './use-cases/get-profile/get-profile.module';
 
 @Module({
-  imports: [SignUpModule, SignInModule],
+  imports: [SignUpModule, SignInModule, GetProfileModule],
 })
 export class AuthModule {}
---
+++ apps/auth/src/modules/auth/use-cases/get-profile/get-profile.service.spec.ts
@@ -0,0 +1,74 @@
+import { Test } from '@nestjs/testing';
+import { GetProfileModule } from './get-profile.module';
+import { CreateUserService } from '@apps/auth/modules/users/use-cases/create-user/create-user.service';
+import { UsersFactory } from '@apps/auth/modules/users/factories/users.factory';
+import { User } from '@apps/auth/modules/users/entities/users.entity';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { CreateUserDto } from '@apps/auth/modules/users/use-cases/create-user/dtos/create-user.dto';
+import { ConfigModule } from '@nestjs/config';
+import { configModuleOptions } from '@apps/auth/modules/configs/config-module.config';
+import { typeOrmModuleOptions } from '@apps/auth/modules/configs/typeorm-config.config';
+import { CreateUserModule } from '@apps/auth/modules/users/use-cases/create-user/create-user.module';
+import { UserDoesNotExistError } from './errors/user-does-not-exist.error';
+import { GetProfileService } from './get-profile.service';
+
+describe(GetProfileService.name, () => {
+  const method = 'execute';
+  const factory = new UsersFactory();
+  const createUserDto: CreateUserDto = factory
+    .pick(['email', 'password', 'firstName', 'lastName'])
+    .makeOne();
+  let service: GetProfileService;
+  let userInDatabase: User;
+
+  beforeAll(async () => {
+    const modules = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(configModuleOptions),
+        TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+
+        CreateUserModule,
+        GetProfileModule,
+      ],
+    }).compile();
+
+    service = modules.get(GetProfileService);
+    const createUserService = modules.get(CreateUserService);
+    const userRepository = modules.get(getRepositoryToken(User));
+
+    await createUserService.create(createUserDto);
+
+    userInDatabase = await userRepository.findOneBy({
+      email: createUserDto.email,
+    });
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe(method, () => {
+    it('should be defined', () => {
+      expect(service[method]).toBeDefined();
+    });
+
+    it("should retrieve the target user's profile", async () => {
+      const spy = jest.spyOn(service, method);
+      const { id, email, firstName, lastName } = userInDatabase;
+      const result = await service.execute(id);
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(result).toMatchObject({ id, email, firstName, lastName });
+    });
+
+    it('should throw error if user does not exist', async () => {
+      const spy = jest.spyOn(service, method);
+      const idNotInDatabase = userInDatabase.id + 1;
+
+      expect(service.execute(idNotInDatabase)).rejects.toThrow(
+        UserDoesNotExistError,
+      );
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
+});
