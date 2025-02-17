32ca5c09bda60519ad065d134b5dca0c1419a5eb
Grecco, Franco <srgrecco93@gmail.com>
2025-02-17 17:10:54 +0100

    feat(auth): add sign-in module to auth module

--- apps/auth/src/auth/auth.module.ts
+++ apps/auth/src/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SignUpModule } from './use-cases/sign-up/sign-up.module';
 import { VerifyEmailModule } from './use-cases/verify-email/verify-email.module';
-// import { SignInModule } from './use-cases/sign-in/sign-in.module';
+import { SignInModule } from './use-cases/sign-in/sign-in.module';
 // import { GetProfileModule } from './use-cases/get-profile/get-profile.module';
 
 @Module({
   imports: [
     SignUpModule,
     VerifyEmailModule,
-    // SignInModule,
+    SignInModule,
     // GetProfileModule
   ],
 })
